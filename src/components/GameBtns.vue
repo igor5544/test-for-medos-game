e7111f13bb07949b13e60fbf3c0cdf0c4a34d71d
igor5544 <igor5544@bk.ru>
2020-09-13 02:24:48 +0500

    project init

---
+++ src/components/GameBtns.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <button
+          class="game-btn game-btn--blue"
+          v-on:click="playSound(sounds[0]), $emit('check-btn', 0)"
+          v-bind:disabled="gameBtnsDisabled"
+        ></button>
+      </li>
+      <li>
+        <button
+          class="game-btn game-btn--red"
+          v-on:click="playSound(sounds[1]), $emit('check-btn', 1)"
+          v-bind:disabled="gameBtnsDisabled"
+        ></button>
+      </li>
+      <li>
+        <button
+          class="game-btn game-btn--yellow"
+          v-on:click="playSound(sounds[2]), $emit('check-btn', 2)"
+          v-bind:disabled="gameBtnsDisabled"
+        ></button>
+      </li>
+      <li>
+        <button
+          class="game-btn game-btn--green"
+          v-on:click="playSound(sounds[3]), $emit('check-btn', 3)"
+          v-bind:disabled="gameBtnsDisabled"
+        ></button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    gameBtnsDisabled: Boolean,
+    sounds: Array,
+    playSound: Function
+  },
+};
+</script>
+
+<style scoped lang="scss">
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  width: 302px;
+  height: 295px;
+  padding: 0;
+  padding-bottom: 5px;
+  margin: 0;
+  border-radius: 50%;
+  box-shadow: 2px 1px 12px #aaa;
+}
+
+li {
+  flex-basis: 49%;
+  height: 145px;
+
+  &:nth-of-type(1) button {
+    border-top-left-radius: 100%;
+  }
+
+  &:nth-of-type(1) button:hover {
+    border-left: 2px solid #000;
+    border-top: 2px solid #000;
+  }
+
+  &:nth-of-type(2) button {
+    border-top-right-radius: 100%;
+  }
+
+  &:nth-of-type(2) button:hover {
+    border-right: 2px solid #000;
+    border-top: 2px solid #000;
+  }
+
+  &:nth-of-type(3) button {
+    border-bottom-left-radius: 100%;
+  }
+
+  &:nth-of-type(3) button:hover {
+    border-left: 2px solid #000;
+    border-bottom: 2px solid #000;
+  }
+
+  &:nth-of-type(4) button {
+    border-bottom-right-radius: 100%;
+  }
+
+  &:nth-of-type(4) button:hover {
+    border-right: 2px solid #000;
+    border-bottom: 2px solid #000;
+  }
+}
+
+.game-btn {
+  width: 100%;
+  height: 100%;
+  border: none;
+  opacity: 0.6;
+
+  &--blue {
+    background-color: $blue-btn;
+  }
+
+  &--red {
+    background-color: $red-btn;
+  }
+
+  &--yellow {
+    background-color: $yellow-btn;
+  }
+
+  &--green {
+    background-color: $green-btn;
+  }
+
+  &--active,
+  &:active {
+    opacity: 1;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
---
+++ src/components/GameOptions.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <label for="easy">
+      <input
+        type="radio"
+        id="easy"
+        name="difficulty"
+        checked
+        v-on:change="$emit('change-delay', difficulty.easy)"
+      />
+      Easy
+    </label>
+    <label for="normal">
+      <input
+        type="radio"
+        id="normal"
+        name="difficulty"
+        v-on:change="$emit('change-delay', difficulty.normal)"
+      />
+      Normal
+    </label>
+    <label for="hard">
+      <input
+        type="radio"
+        id="hard"
+        name="difficulty"
+        v-on:change="$emit('change-delay', difficulty.hard)"
+      />
+      Hard
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      difficulty: {
+        easy: 1500,
+        normal: 1000,
+        hard: 400
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+label {
+  display: block;
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+input {
+  margin-right: 7px;
+}
+</style>
